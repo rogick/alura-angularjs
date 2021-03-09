8c1c7cbe9cf73f2d4e85cb1183490621716c0e43
Rogick Manoel <rogick.manoel@sankhya.com.br>
2021-03-09 09:28:43 -0300

    Aula 4 - Criando propria diretiva

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,14 @@
+angular.module('minhasDiretivas', [])
+    .directive('meuPainel', function(){
+        var ddo = {};
+
+        ddo.restrict = "AE";
+        ddo.scope = {
+            titulo: '@'
+        }
+        ddo.transclude = true;
+        
+
+        ddo.templateUrl = 'js/directives/meu-painel.html';
+        return ddo;
+    })
